0be53a4c7aac444648ff857bbc4d8e1a3849c2f9
rladm <rladm@LAPTOP-I2NUJV7B>
2023-01-25 16:47:05 -0700

    Add more code on Arithmetic Calculator Added the code result for "---", when no calculation has been performed.

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -13,6 +13,7 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setAttribute("result","---");
         getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
                 .forward(request,response);   
     }
